4d096b84e30493f0448684687bc6aec30a85e90e
Matheus Romaneli <matheusromaneli@id.uff.br>
2021-08-28 20:28:38 -0300

    erease/write and fill space for speed input

--- main.py
+++ main.py
@@ -39,7 +39,7 @@ def plot_circle_around(window,position,radius, color = (255,0,0), to_fill = Fals
     for x in range(size_map):
         (plus,minus) = circle_form_around(x,radius,position[0],position[1],window)
         if to_fill:
-            for i in range(int(minus),int(plus),2):
+            for i in range(int(minus),int(plus),1):
                 if i > 0:
                     window.blit(point,(x,i))
         else:
@@ -50,34 +50,54 @@ def plot_circle_around(window,position,radius, color = (255,0,0), to_fill = Fals
         
 
 
-radius = 100
+radius = 5
 color = (255,0,0)
-filler = False
+filler = True
+can_draw = False
+last_pos =(-1,-1)
 while(1):
     # window.fill((0,0,0))
 
-    pos = pygame.mouse.get_pos()
-    pygame.event.get()
+    events = pygame.event.get()
+    for event in events:
+        pos = pygame.mouse.get_pos()
+        button = pygame.mouse.get_pressed(num_buttons=5)
+        if button [0]: #esquerdo
+            can_draw = True
+        else:
+            can_draw = False
 
-    button = pygame.mouse.get_pressed(num_buttons=5)
-    if button [0]: #esquerdo
-        can_draw = True
-    else:
-        can_draw = False
+        if button [1]:#meio
+            color = (random.randint(0,255) ,random.randint(0,255),random.randint(0,255))
+        elif button [2] and pygame.MOUSEBUTTONDOWN: # direito
+            filler = not(filler)
+        elif button[4]: #botao lateral
+            radius += 1
+        elif button[3]:#botao lateral
+            if radius > 1 :
+                radius -= 1
+        if event.type == pygame.KEYDOWN:
+            pressed_keys = pygame.key.get_pressed()
+            if pressed_keys[pygame.K_q]:
+                color = (0,0,0)
+            if pressed_keys[pygame.K_e]:
+                color = (255,255,255)
 
-    if button [1]:#meio
-        color = (random.randint(0,255) ,random.randint(0,255),random.randint(0,255))
-    elif button [2] and pygame.MOUSEBUTTONDOWN: # direito
-        filler = not(filler)
-    elif button[4]: #botao lateral
-        radius += 1
-    elif button[3]:#botao lateral
-        if radius >0:
-            radius -= 1
-
-    if(can_draw):
-        plot_circle_around(window,pos,radius,color,filler)
-
+        if can_draw:
+            if last_pos[0] != pos[0] or last_pos[1] != pos[1]:
+                vetor = (pos[0] - last_pos[0], pos[1] - last_pos[1])
+                print(vetor)
+                norma_vetor = int(math.sqrt(pow(vetor[0], 2) + pow(vetor[1],2)))
+                print("A NORMA E: ", norma_vetor)
+                if norma_vetor:
+                    # aux_pos = [last_pos[0]+ (vetor[0]), last_pos[1]+ (vetor[1])]
+                    aux_pos = [last_pos[0], last_pos[1]]
+                    for i in range(norma_vetor,1,-1):
+                        aux_pos[0] += vetor[0]/norma_vetor
+                        aux_pos[1] += vetor[1]/norma_vetor
+                        plot_circle_around(window,aux_pos,radius,color,filler)
+            plot_circle_around(window,pos,radius,color,filler)
+        last_pos = pos
     # plot_circle(window,1)
     pygame.display.update()
     # time.sleep(1)
